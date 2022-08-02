c4740af2b10bdd4f56a23a89213ebeb604258888
Anna Dolejší <dolejsianna@email.cz>
2022-08-02 09:59:36 +0200

    vyreseny ukoly

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("JavaScript funguje!");
+
+let vzdelaniNaSkoly = document.querySelector(".vzdelani");
+vzdelaniNaSkoly.textContent = "Školy";
+
+let barvaOdstavce = document.querySelector(".predstaveni-se");
+barvaOdstavce.style.color = "navy";
+
+let obrazekLary = document.querySelector(".moje-foto");
+obrazekLary.src = "img/druhe-foto.jpeg";
+
+let nastavTridu = document.querySelector(".tlacitko1");
+nastavTridu.className = "zvyrazneny";
